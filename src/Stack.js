4f9a592bea22b51517dee493b8f412211b493fcb
Johnny <jkfaris@gmail.com>
2025-07-22 10:47:23 -0700

    added push() and pop() methods

--- src/Stack.js
+++ src/Stack.js
@@ -10,4 +10,21 @@ class Stack {
   constructor() {
     this.top = null;
   }
+
+    push(value) {
+    // Create a new node,
+    // add data to the new node, and
+    // have the pointer point to the top
+    this.top = new Node(value, this.top);
+    return this;
+  }
+
+  pop() {
+      /* In order to remove the top of the stack, you have to point
+          the pointer to the next item. That next item becomes the
+          top of the stack */
+      const popped = this.top;
+      this.top = popped.next;
+      return popped.value;
+  }
 }
