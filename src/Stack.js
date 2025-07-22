9173878398a587ffd52d9c1294d49a7440718167
Johnny <jkfaris@gmail.com>
2025-07-22 10:45:24 -0700

    created stack class and defined constructor

---
+++ src/Stack.js
@@ -0,0 +1,13 @@
+// Creates a node containing the data and a reference to the next item
+class Node {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+  }
+}
